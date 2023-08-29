2ceb97337354834b1a30f61b2a8a429d789675c1
haruto <harutai0602@gmail.com>
2023-08-29 22:12:07 +0900

    fix button

--- action/pointget.js
+++ action/pointget.js
@@ -1,7 +1,11 @@
-const points = localStorage.getItem('selectedPoints');
+/*const points = localStorage.getItem('selectedPoints');
 document.getElementById('pointDisplay').textContent += selectedPoints;
 
 const homeButton = document.getElementById('homeButton');
 homeButton.addEventListener('click', function () {
     window.location.href = '../index.html';
-});
+});*/
+
+const selectedPoints = localStorage.getItem('selectedPoints');
+const text = document.getElementById('pointDisplay').textContent
+document.getElementById('pointDisplay').textContent = text + selectedPoints
