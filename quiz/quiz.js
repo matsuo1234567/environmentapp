4fb968cb0048d661804c3ee3718e083c33b46b07
Haruki <t.haruki160427@gmail.com>
2023-08-29 22:10:22 +0900

    fix.color

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,25 +1,29 @@
-const questionElement=
-document.getElementById('question');
-const choicesElement=
-document.getElementById('choices');
-const choice1Element=
-document.getElementById('choice1');
-const choice2Element=
-document.getElementById('choice2');
-const choice3Element=
-document.getElementById('choice3');
-
-
-
-const quizedate=[
-    {
-        question:'日本の首都はどこでしょうか？',
-        choices:['東京', '京都','大阪'],
-        correctchoices:0
+const quizs = {
+        q:"もんだいもんだいもんだい",
+        c:"1.正解のこたえ",
+        i1:"2.不正解のこたえ１",
+        i2:"3.不正解のこたえ２",
     }
-]
 
 
-let currentQuestionIndex=0;
+document.getElementById("question")
+const questionElement=document.getElementById("question")
+questionElement.textContent=quizs.q
 
+document.getElementById("choice1")
+const choice1Element=document.getElementById("choice1")
+choice1Element.textContent=quizs.c
+
+document.getElementById("choice2")
+const choice2Element=document.getElementById("choice2")
+choice2Element.textContent=quizs.i1
+
+document.getElementById("choice3")
+const choice3Element=document.getElementById("choice3")
+choice3Element.textContent=quizs.i2
+
+function seikai(){
+    location.href="/quiz/answer/true.html"
+}
+choice1Element.addEventListener("click",seikai)
 
