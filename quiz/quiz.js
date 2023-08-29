4f5d0965bea5d01460a23649caf243de3d51f59a
Haruki <t.haruki160427@gmail.com>
2023-08-29 18:42:24 +0900

    追加

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,1 +1,25 @@
-console.log("quiz");
+const questionElement=
+document.getElementById('question');
+const choicesElement=
+document.getElementById('choices');
+const choice1Element=
+document.getElementById('choice1');
+const choice2Element=
+document.getElementById('choice2');
+const choice3Element=
+document.getElementById('choice3');
+
+
+
+const quizedate=[
+    {
+        question:'日本の首都はどこでしょうか？',
+        choices:['東京', '京都','大阪'],
+        correctchoices:0
+    }
+]
+
+
+let currentQuestionIndex=0;
+
+
